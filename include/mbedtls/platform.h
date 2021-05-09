8675b5b08831a79904c6f69e9bd0152941255fe1
Afa Cheng <afa@afa.moe>
2021-05-09 22:11:50 +0800

    tbb malloc for mbedtls

--- include/mbedtls/platform.h
+++ include/mbedtls/platform.h
@@ -154,8 +154,9 @@ int mbedtls_platform_set_calloc_free( void * (*calloc_func)( size_t, size_t ),
                               void (*free_func)( void * ) );
 #endif /* MBEDTLS_PLATFORM_FREE_MACRO && MBEDTLS_PLATFORM_CALLOC_MACRO */
 #else /* !MBEDTLS_PLATFORM_MEMORY */
-#define mbedtls_free       free
-#define mbedtls_calloc     calloc
+#include <tbb/scalable_allocator.h>
+#define mbedtls_free       scalable_free
+#define mbedtls_calloc     scalable_calloc
 #endif /* MBEDTLS_PLATFORM_MEMORY && !MBEDTLS_PLATFORM_{FREE,CALLOC}_MACRO */
 
 /*
